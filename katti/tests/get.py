071319bacb78b0ffe89978ea2615ed5b0fbeb847
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-05-14 18:34:15 -0500

    ran autopep8

--- katti/tests/get.py
+++ katti/tests/get.py
@@ -1,4 +1,6 @@
 import requests
+
+
 def main():
     username = 'matthew-freestone'
     token = "7412e41f4f38ccfcf61c933ba882c95893943def3dc98b2ac4d76e8f0e7d2d3a"
@@ -10,7 +12,8 @@ def main():
     }
     login_response = requests.post(url + "/login", data=creds)
 
-    response = requests.get(url + "/submissions/11015735", cookies=login_response.cookies)
+    response = requests.get(url + "/submissions/11015735",
+                            cookies=login_response.cookies)
     with open("./submission_get_response.txt", 'w') as f:
         f.write(response.text)
     print(response.status_code)
